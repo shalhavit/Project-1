85824909343640e84207b873070e20c6a4be325c
shalhavit <shalhavit@gmail.com>
2014-02-12 20:44:28 -0500

    Changed the time in order to contain full length of my video

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ function updateWaypoints() {
 	}
 
 	// Seek to the proportional time of the 38s clip of Bey's "Countdown"
-	document.getElementById('Countdown').currentTime = fractionScrolled * 38.0;
+	document.getElementById('Countdown').currentTime = fractionScrolled * 47.0;
 }
 function triangleClickHandler(e) {
  	console.log('Tclick');
@@ -47,7 +47,7 @@ function triangleClickHandler(e) {
  			updateWaypoints();
 			console.log(scrolled);
 }
-
+}
 
 function waypointClickHandler(e) {
 	console.log('cilck');
